a3f239ecd54d233620fe8900d969545afacdea99
DuocAnh <duocanh14@gmail.com>
2025-03-27 17:21:34 +0700

    query board owner member

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -6,6 +6,7 @@ import { BOARD_TYPES } from '~/utils/constants'
 import { columnModel } from './columnModel'
 import { cardModel } from './cardModel'
 import { pagingSkipValue } from '~/utils/algorithms'
+import { userModel } from './userModel'
 
 // Define Collection (name & schema)
 const BOARD_COLLECTION_NAME = 'boards'
@@ -94,7 +95,27 @@ const getDetails = async (userId, boardId) => {
         localField: '_id',
         foreignField: 'boardId',
         as: 'cards'
-      } }
+      } },
+      {
+        $lookup: {
+          from: userModel.USER_COLLECTION_NAME,
+          localField: 'ownerIds',
+          foreignField: '_id',
+          as: 'owners',
+          // pipeline trong lookup là để xử lý một hoặc nhiều luồng cần thiết
+          // $project để chỉ định vài field không muốn lấy về bằng cách gán nó giá trị 0
+          pipeline: [{ $project: { 'password': 0, 'verifyToken': 0 } }]
+        }
+      },
+      {
+        $lookup: {
+          from: userModel.USER_COLLECTION_NAME,
+          localField: 'memberIds',
+          foreignField: '_id',
+          as: 'members',
+          pipeline: [{ $project: { 'password': 0, 'verifyToken': 0 } }]
+        }
+      }
     ]).toArray()
 
     return result[0] || null
