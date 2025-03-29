379d5ff5db5486e9f063a094165e7caa49fa730a
DuocAnh <duocanh14@gmail.com>
2025-03-29 18:59:30 +0700

    join and remove card member

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -43,6 +43,8 @@ const update = async (cardId, reqBody, cardCoverFile, userInfo) => {
         commentedAt: Date.now()
       }
       updatedCard = await cardModel.unshiftNewComment(cardId, commentData)
+    } else if (updateData.incomingMemberInfo) {
+      updatedCard = await cardModel.updateMembers(cardId, updateData.incomingMemberInfo)
     } else {
       // update title, description
       updatedCard = await cardModel.update(cardId, updateData)
